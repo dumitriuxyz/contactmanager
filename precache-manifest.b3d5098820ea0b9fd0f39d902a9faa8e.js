0059ca8de84d68a417d8a96123a3f75eccedf1c2
dumitriuxyz <dragos@dumitriu.xyz>
2018-11-15 12:24:13 +0200

    Updates

---
+++ precache-manifest.b3d5098820ea0b9fd0f39d902a9faa8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "8e6c5e7ae24f1f8ba773",
+    "url": "/contactmanager/static/js/main.8e6c5e7a.chunk.js"
+  },
+  {
+    "revision": "59dc6934e59925e8cd4b",
+    "url": "/contactmanager/static/js/1.59dc6934.chunk.js"
+  },
+  {
+    "revision": "8e6c5e7ae24f1f8ba773",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "59dc6934e59925e8cd4b",
+    "url": "/contactmanager/static/css/1.02c4d684.chunk.css"
+  },
+  {
+    "revision": "bdac73cfa3b10a62b7bd96a8c70d6207",
+    "url": "/contactmanager/index.html"
+  }
+];
